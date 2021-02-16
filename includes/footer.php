616c884a8e95291eab9c6dcbc5f7aad1ddbe51cd
OLUOKUN KABIRU ADESINA <villageboy@villageboy.villageboy.com>
2021-02-16 09:04:00 +0100

    change footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <div class="row">
                             <div class="col-md-12">
                                 <div class="copyright">
-                                    <p>CSE 405 Dr. Mrs. Adetunji &copy; <?php echo date('Y');?>. All rights reserved by Group 3 supervised .</p>
+                                    <p>CSE 405 Done by Group 3 and supervised by Dr. Mrs. Adetunji &copy; <?php echo date('Y');?>. All rights reserved </p>
                                 </div>
                             </div>
                         </div>
