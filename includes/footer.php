9eaef1954a5c8cf933012e2337b81f55994aba7a
OLUOKUN KABIRU ADESINA <villageboy@villageboy.villageboy.com>
2021-02-16 04:32:50 +0100

    remove my details from the project

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <div class="row">
                             <div class="col-md-12">
                                 <div class="copyright">
-                                    <p> Group 3 supervised by Dr. Mrs. Adetunji &copy; <?php echo date('Y');?>. All rights reserved.</p>
+                                    <p>CSE 405 Dr. Mrs. Adetunji &copy; <?php echo date('Y');?>. All rights reserved by Group 3 supervised .</p>
                                 </div>
                             </div>
                         </div>
